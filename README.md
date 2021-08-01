50271f482e7b8c615c2fe3b11ba3c3568144e092
Max <maxabaum@gmail.com>
2021-08-01 16:41:16 -0400

    commitg

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # scrape-challenge
-A challenge to scrap http://books.toscrape.com/ 
+
+A challenge to scrape http://books.toscrape.com/ 
+
+## Details
+
+- Written in Python
+- Parses product information from the Science and Poetry categories
+- Saves data in a csv file
+
+## Requirements
+
+```sh
+pip install -r requirements.txt
+```
+
+## Run
+
+```sh
+python main.py
+```
+View the generated CSV file for the data
+
+## Issues
+
+- This currently does not work
+- Issue is due to not understanding how BeautifulSoup parses href links.
+- Currently the program is returning "..//../" instead of the actual links
---
+++ main.py
@@ -0,0 +1,63 @@
+  
+###########################################################
+############## Created by Max Baumgarten ##################
+###########################################################
+
+# import required libraries
+
+import csv
+from bs4 import BeautifulSoup
+import requests
+import json
+
+# create csv file to store data
+filename = 'data.csv'
+f = open(filename, 'w')
+
+# science catergory url
+science_url = "http://books.toscrape.com/catalogue/category/books/science_22/index.html"
+
+# get each book url from science_url
+response = requests.get(science_url)
+soup = BeautifulSoup(response.text, "html.parser")
+book_urls = soup.find_all("article", class_="product_pod")
+book_urls = [book.find("a")["href"] for book in book_urls]
+
+# loop through each book url
+for book_url in book_urls:
+
+    # get name, price, Product Description, UPC, Product Type, Tax, Number of reviews of book and store in csv file
+    response = requests.get(book_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    name = soup.find("h1", class_="product_name").text.strip()
+    price = soup.find("p", class_="price_color").text.strip()
+    product_description = soup.find("div", class_="product_description").text.strip()
+    upc = soup.find("span", class_="upc").text.strip()
+    product_type = soup.find("p", class_="product_type").text.strip()
+    tax = soup.find("p", class_="tax").text.strip()
+    number_of_reviews = soup.find("p", class_="star-rating").text.strip()
+    f.write(name + "," + price + "," + product_description + "," + upc + "," + product_type + "," + tax + "," + number_of_reviews + "\n")
+
+# poetry catergory url
+poetry_url = "http://books.toscrape.com/catalogue/category/books/science_22/index.html"
+
+# get each book url from science_url
+response = requests.get(poetry_url)
+soup = BeautifulSoup(response.text, "html.parser")
+book_urls = soup.find_all("article", class_="product_pod")
+book_urls = [book.find("a")["href"] for book in book_urls]
+
+# loop through each book url
+for book_url in book_urls:
+
+    # get name, price, Product Description, UPC, Product Type, Tax, Number of reviews of book and store in csv file
+    response = requests.get(book_url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    name = soup.find("h1", class_="product_name").text.strip()
+    price = soup.find("p", class_="price_color").text.strip()
+    product_description = soup.find("div", class_="product_description").text.strip()
+    upc = soup.find("span", class_="upc").text.strip()
+    product_type = soup.find("p", class_="product_type").text.strip()
+    tax = soup.find("p", class_="tax").text.strip()
+    number_of_reviews = soup.find("p", class_="star-rating").text.strip()
+    f.write(name + "," + price + "," + product_description + "," + upc + "," + product_type + "," + tax + "," + number_of_reviews + "\n")
